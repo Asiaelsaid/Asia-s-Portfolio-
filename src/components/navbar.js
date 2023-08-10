ace6e6f1763b99b89b824a0e9745e783bf0f3b5f
Asiaelsaid <126247042+Asiaelsaid@users.noreply.github.com>
2023-08-10 17:35:49 +0300

    Add files via upload

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -120,7 +120,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="https://drive.google.com/file/d/1olf0URbNvDrFIW2lopP9hmmSh3N3JxYO/view?usp=drive_link"
+              href="https://drive.google.com/drive/folders/1PUEQZM1oxcqmrNyg4HIaRXl5PCwciG3W"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
